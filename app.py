e08f10c935437daacf4e913ac9ebbbe3895c9a84
Weiwei <w1216lin@gmail.com>
2023-11-27 01:35:15 -0500

    implemented pictures service

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ from backend import app
 if __name__ == '__main__':  # Script executed directly?
     print("pictures microservice.")
     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
+    
--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return data
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +44,10 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    for pic in data:
+        if pic["id"] == id:
+            return pic
+    return {"Message": "picture not found"}, 404
 
 
 ######################################################################
@@ -52,7 +55,21 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    new_pic = request.json
+
+    if not new_pic:
+        return {"message": "Invalid input parameter"}, 422
+
+    for pic in data:
+        if pic['id'] == new_pic['id']:
+            return {"Message": f"picture with id {pic['id']} already present"}, 302
+
+    try:
+        data.append(new_pic)
+    except NameError:
+        return {"Message": "data not defined"}, 500
+
+    return {"id": new_pic['id']}, 201
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +78,25 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    pic_data = request.json
+    if not pic_data:
+        return {"message": "Invalid input parameter"}, 422
+    
+    for pic in data:
+        if pic['id'] == pic_data['id']:
+            pic.update(pic_data)
+            return {"id": pic['id']}
+    return {"message": "picture not found"}, 404
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for pic in data:
+        if pic["id"] == id:
+            data.remove(pic)
+            return {"id": id}, 204
+
+    return {"message": "picture not found"}, 404
